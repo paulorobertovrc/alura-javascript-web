e2aee84dc35ec81dfceb1883c2d4bd5c65d1038b
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-19 10:11:24 -0400

    Javascript: manipulando o DOM - Aula 04

--- robotron/js/main.js
+++ robotron/js/main.js
@@ -1,27 +1,18 @@
-// const subtrair = document.querySelector('#subtrair');
-// const somar = document.querySelector('#somar');
-// const braco = document.querySelector('#braco');
-
-const controle = document.querySelectorAll('.controle-ajuste');
+const controle = document.querySelectorAll('[data-controle]');
 
 controle.forEach((elemento) => {
-    elemento.addEventListener('click', () => {
-        let equipamento = elemento.parentElement.getAttribute('id');
-        let operacao = elemento.getAttribute('id');
-
-        manipularDados(equipamento, operacao);
+    elemento.addEventListener('click', (evento) => {
+        manipularDados(evento.target.dataset.controle, evento.target.parentNode);
     });
 });
 
-function manipularDados(equipamento, operacao) {
-    let componente = document.querySelector(`#${equipamento}`).querySelector('input');
-    let valor = componente.getAttribute('value');
+function manipularDados(operacao, controle) {
+    const peca = controle.querySelector('[data-contador]');
     
-    if (operacao === 'somar') {
-        valor = parseInt(valor) + 1;
+    if (operacao === '-') {
+        peca.value = parseInt(peca.value) - 1;
     } else {
-        valor = parseInt(valor) - 1;
+        peca.value = parseInt(peca.value) + 1;
     }
 
-    componente.setAttribute('value', valor);
 }

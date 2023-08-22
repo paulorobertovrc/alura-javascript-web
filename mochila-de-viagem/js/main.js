1b4528ed07efd3f6f7f3a0637aa9966e98372e4c
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-22 18:36:10 -0400

    Javascript na web - Aula 02

--- mochila-de-viagem/js/main.js
+++ mochila-de-viagem/js/main.js
@@ -1,13 +1,17 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
+const itens = [];
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
     
-    const nome = evento.target.elements['nome'].value;
-    const quantidade = evento.target.elements['quantidade'].value;
+    const nome = evento.target.elements['nome'];
+    const quantidade = evento.target.elements['quantidade'];
 
-    criarElemento(nome, quantidade);
+    criarElemento(nome.value, quantidade.value);
+    
+    nome.value = '';
+    quantidade.value = '';
 });
 
 function criarElemento(nome, quantidade) {
@@ -21,4 +25,13 @@ function criarElemento(nome, quantidade) {
     novoItem.innerHTML += nome;
 
     lista.appendChild(novoItem);
+
+    const itemAtual = {
+        nome: nome,
+        quantidade: quantidade
+    }
+
+    itens.push(itemAtual);
+
+    localStorage.setItem('item', JSON.stringify(itens));
 }

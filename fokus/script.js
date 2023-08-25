1f5af5a31f35c39ef2fdcd62eb4cd4a77d97d099
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-25 18:25:48 -0400

    Manipulando elementos no DOM (novo) - Aula 03

--- fokus/script.js
+++ fokus/script.js
@@ -1,27 +1,43 @@
 const html = document.querySelector('html');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('sons/luna-rise-part-one.mp3');
 
 const tipoFoco = document.querySelectorAll('[data-botaoTipoFoco]');
 const focoBt = tipoFoco[0];
 const curtoBt = tipoFoco[1];
 const longoBt = tipoFoco[2];
 
+musica.loop = true;
+musicaFocoInput.addEventListener('change', () => {
+    if (musicaFocoInput.checked) {
+        musica.play();
+    } else {
+        musica.pause();
+    }
+});
+
 focoBt.addEventListener('click', () => {
-    alterarContexto('foco');
+    alterarContexto('foco', focoBt);
 });
 
 curtoBt.addEventListener('click', () => {
-    alterarContexto('descanso-curto');
+    alterarContexto('descanso-curto', curtoBt);
 });
 
 longoBt.addEventListener('click', () => {
-    alterarContexto('descanso-longo');
+    alterarContexto('descanso-longo', longoBt);
 });
 
-function alterarContexto(contexto) {
+function alterarContexto(contexto, botao) {
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `imagens/${contexto}.png`);
+    
+    tipoFoco.forEach((bt) => {
+        bt.classList.remove('active');
+    });
+    botao.classList.add('active');
 
     switch (contexto) {
         case 'foco':

ed8936e9d30743a3d6d6c478350456b620dd5ca6
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-27 10:23:09 -0400

    Manipulando elementos no DOM (novo) - Aula 04

--- fokus/script.js
+++ fokus/script.js
@@ -1,13 +1,22 @@
 const html = document.querySelector('html');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+
 const musicaFocoInput = document.querySelector('#alternar-musica');
 const musica = new Audio('sons/luna-rise-part-one.mp3');
+const somPlay = new Audio('sons/play.wav');
+const somPause = new Audio('sons/pause.mp3');
+const somFim = new Audio('sons/beep.mp3');
 
 const tipoFoco = document.querySelectorAll('[data-botaoTipoFoco]');
 const focoBt = tipoFoco[0];
 const curtoBt = tipoFoco[1];
 const longoBt = tipoFoco[2];
+
+const startPauseBt = document.querySelector('#start-pause');
+
+let tempoDecorridoEmSegundos = 5;
+let intervaloId = null;
 
 musica.loop = true;
 musicaFocoInput.addEventListener('change', () => {
@@ -56,4 +65,34 @@ function alterarContexto(contexto, botao) {
             break;
     }
 
+}
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        somFim.play();
+        alert('Acabou!');
+        pararTemporizador();
+        return;
+    }
+
+    tempoDecorridoEmSegundos -= 1;
+    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+}
+
+startPauseBt.addEventListener('click', iniciarTemporizador);
+
+function iniciarTemporizador() {
+    if (intervaloId) {
+        somPause.play();
+        pararTemporizador();
+        return;
+    }
+    
+    somPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000);
+}
+
+function pararTemporizador() {
+    clearInterval(intervaloId);
+    intervaloId = null;
 }

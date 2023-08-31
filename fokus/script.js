07a243d975383ddd7def5078742a1dcc323a1349
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-08-31 16:24:03 -0400

    Manipulando elementos no DOM (novo) - Aula 05

--- fokus/script.js
+++ fokus/script.js
@@ -14,8 +14,12 @@ const curtoBt = tipoFoco[1];
 const longoBt = tipoFoco[2];
 
 const startPauseBt = document.querySelector('#start-pause');
+const startPauseBtTexto = document.querySelector('#start-pause span');
+const startPauseImg = document.querySelector('#start-pause img');
 
-let tempoDecorridoEmSegundos = 5;
+const tempoRestante = document.querySelector('#timer');
+
+let tempoDecorridoEmSegundos = 1500;
 let intervaloId = null;
 
 musica.loop = true;
@@ -28,18 +32,22 @@ musicaFocoInput.addEventListener('change', () => {
 });
 
 focoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 1500;
     alterarContexto('foco', focoBt);
 });
 
 curtoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 300;
     alterarContexto('descanso-curto', curtoBt);
 });
 
 longoBt.addEventListener('click', () => {
+    tempoDecorridoEmSegundos = 900;
     alterarContexto('descanso-longo', longoBt);
 });
 
 function alterarContexto(contexto, botao) {
+    exibirTemporizador();
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `imagens/${contexto}.png`);
     
@@ -72,11 +80,13 @@ const contagemRegressiva = () => {
         somFim.play();
         alert('Acabou!');
         pararTemporizador();
+        startPauseBtTexto.textContent = 'Reiniciar';
+        startPauseImg.setAttribute('src', 'imagens/play_arrow.png');
         return;
     }
 
     tempoDecorridoEmSegundos -= 1;
-    console.log('Temporizador: ' + tempoDecorridoEmSegundos);
+    exibirTemporizador();
 }
 
 startPauseBt.addEventListener('click', iniciarTemporizador);
@@ -84,15 +94,29 @@ startPauseBt.addEventListener('click', iniciarTemporizador);
 function iniciarTemporizador() {
     if (intervaloId) {
         somPause.play();
+        startPauseBtTexto.textContent = 'Retomar';
+        startPauseImg.setAttribute('src', 'imagens/play_arrow.png');
         pararTemporizador();
         return;
     }
     
     somPlay.play();
     intervaloId = setInterval(contagemRegressiva, 1000);
+    startPauseBtTexto.textContent = 'Pausar';
+    startPauseImg.setAttribute('src', 'imagens/pause.png');
 }
 
 function pararTemporizador() {
     clearInterval(intervaloId);
     intervaloId = null;
 }
+
+function exibirTemporizador() {
+    const tempo = new Date(tempoDecorridoEmSegundos * 1000).toLocaleTimeString('pt-BR', {
+        minute: '2-digit',
+        second: '2-digit',
+    });
+    tempoRestante.innerHTML = `${tempo}`;
+}
+
+exibirTemporizador();

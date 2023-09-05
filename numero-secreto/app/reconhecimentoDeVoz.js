ff2f98484b7103a3172f36ba8b00094a740afaba
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-09-04 20:27:06 -0400

    Validações e reconhecimento de voz - Aula 03

---
+++ numero-secreto/app/reconhecimentoDeVoz.js
@@ -0,0 +1,19 @@
+const elementoChute = document.querySelector('#chute');
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-BR';
+recognition.start();
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak(e) {
+    chute = e.results[0][0].transcript;
+    exibirChute(chute);
+}
+
+function exibirChute(chute) {
+    elementoChute.innerHTML = `
+        <div>Você disse</div>
+        <span class="box">${chute}</span>`;
+}
--- numero-secreto/app/sortearNumero.js
+++ numero-secreto/app/sortearNumero.js
@@ -4,11 +4,9 @@ const numeroSecreto = gerarNumeroAleatorio();
 
 const elementoMenorValor = document.getElementById("menor-valor");
 const elementoMaiorValor = document.getElementById("maior-valor");
-const elementoNumeroSecreto = document.getElementById("numero-secreto");
 
 elementoMenorValor.innerHTML = menorNumero;
 elementoMaiorValor.innerHTML = maiorNumero;
-elementoNumeroSecreto.innerHTML = numeroSecreto;
 
 function gerarNumeroAleatorio() {
   return Math.round(Math.random() * maiorNumero + 1);

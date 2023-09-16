e878e1fb2913a1d5be9ea13d6de3a4c2abd2857c
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-09-16 13:53:28 -0400

    Consumindo e tratando dados - Finalização do curso

--- consumindo-e-tratando-api/script.js
+++ consumindo-e-tratando-api/script.js
@@ -1,5 +1,28 @@
-const consultaCep = fetch('https://viacep.com.br/ws/01001000/json/')
-    .then(async (response) => {
-        const data = await response.json();
-        console.log(data);
-    });
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = '';
+
+    try {
+        const consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        const resultado = await consultaCep.json();
+        if (resultado.erro) throw new Error('CEP inválido!');
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var estado = document.getElementById('estado');
+
+        cidade.value = resultado.localidade;
+        logradouro.value = resultado.logradouro;
+        bairro.value = resultado.bairro;
+        estado.value = resultado.uf;
+
+        return resultado;
+    } catch (erro) {
+        mensagemErro.innerHTML = erro;
+        console.log(erro);
+    }
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaEndereco(cep.value).then(console.log));

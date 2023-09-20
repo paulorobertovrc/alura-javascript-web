15165b8c4e13f8b1dd56aa09ac5504f0823cf2af
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2023-09-20 18:00:19 -0400

    Métodos de array - Aulas 02 e 03

--- alura-books/app/main.js
+++ alura-books/app/main.js
@@ -1,6 +1,5 @@
 let livros = [];
 const endpoint = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
-const elementoParaInserirLivros = document.getElementById('livros');
 
 getBuscarLivros();
 
@@ -8,20 +7,8 @@ async function getBuscarLivros() {
   const response = await fetch(endpoint);
   const data = await response.json();
   livros = data;
-  console.table(livros);
-  exibirLivros(livros);
-}
 
-function exibirLivros(livros) {
-    livros.forEach(livro => {
-        elementoParaInserirLivros.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}">
-            <h2 class="livro__titulo">${livro.titulo}</h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco">R$ ${livro.preco}</p>
-            <div class="tags"><span class="tag">${livro.categoria}</span></div>
-        </div>
-        `;
-    });
+  let livrosComDesconto = aplicarDesconto(livros);
+
+  exibirLivros(livrosComDesconto);
 }
